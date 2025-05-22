ae5ba4c78cb71a5753a4db09760da5ca3bebeb0b
Alexander Kasten <alexander.kasten@5minds.de>
2025-05-22 20:27:49 +0200

    Fix formatting issues by ensuring newlines at the end of files and maintaining consistent return statements in functions.

--- resources/lib/handler/pluginHandler.py
+++ resources/lib/handler/pluginHandler.py
@@ -291,47 +291,4 @@ class cPluginHandler:
                     t.start()
             except Exception:
                 pass
-        
-        for count, t in enumerate(threads):
-            t.join()
-
         log(LOGMESSAGE + ' -> [checkDomain]: Domains for all available Plugins updated', LOGNOTICE)
-        
-    def _checkdomain(self, provider, base_link):
-        try:
-            oRequest = cRequestHandler(base_link, caching=False, ignoreErrors=True)
-            oRequest.request()
-            status_code = int(oRequest.getStatus())
-            cConfig().setSetting('plugin_' + provider + '_status', str(status_code))  # setzte Status Code in die settings
-            log(LOGMESSAGE + ' -> [checkDomain]: Status Code ' + str(status_code) + '  ' + provider + ': - ' + base_link, LOGNOTICE)
-
-            # Status 403 - bedeutet, dass der Zugriff auf eine angeforderte Ressource blockiert ist.
-            # Status 404 - Seite nicht gefunden. Diese Meldung zeigt an, dass die Seite oder der Ordner auf dem Server, die aufgerufen werden sollten, nicht unter der angegebenen URL zu finden sind.
-            if 403 <= status_code <= 503:  # Domain Interner Server Error und nicht erreichbar
-                cConfig().setSetting('global_search_' + provider, 'false')  # deaktiviere Globale Suche
-                log(LOGMESSAGE + ' -> [checkDomain]: Internal Server Error for ' + provider + ' (DDOS Guard, HTTP Error, Cloudflare or BlazingFast active)', LOGNOTICE)
-
-            # Status 301 - richtet Ihr auf Eurem Server ein, wenn sich die URL geändert hat, Eure Domain umgezogen ist oder sich ein Inhalt anderweitig verschoben hat.
-            elif 300 <= status_code <= 400:  # Domain erreichbar mit Umleitung
-                url = oRequest.getRealUrl()
-                cConfig().setSetting('plugin_' + provider + '.domain', urlparse(url).hostname)  # setze Domain in die settings.xml
-                cConfig().setSetting('global_search_' + provider, 'true')  # aktiviere Globale Suche
-                log(LOGMESSAGE + ' -> [checkDomain]: globalSearch for ' + provider + ' is activated.', LOGNOTICE)
-
-            # Status 200 - Dieser Code wird vom Server zurückgegeben, wenn er den Request eines Browsers korrekt zurückgeben kann. Für die Ausgabe des Codes und des Inhalts der Seite muss der Server die Anfrage zunächst akzeptieren.
-            elif status_code == 200:  # Domain erreichbar
-                cConfig().setSetting('plugin_' + provider + '.domain', urlparse(base_link).hostname)  # setze URL_MAIN in die settings.xml
-                cConfig().setSetting('global_search_' + provider, 'true')  # aktiviere Globale Suche
-                log(LOGMESSAGE + ' -> [checkDomain]: globalSearch for ' + provider + ' is activated.', LOGNOTICE)
-            # Wenn keiner der Status oben greift
-            else:
-                log(LOGMESSAGE + ' -> [checkDomain]: Error ' + provider + ' not available.', LOGNOTICE)
-                cConfig().setSetting('global_search_' + provider, 'false')  # deaktiviere Globale Suche
-                xbmcaddon.Addon().setSetting('plugin_' + provider + '.domain', '')  # lösche Settings Eintrag
-                log(LOGMESSAGE + ' -> [checkDomain]: globalSearch for ' + provider + ' is deactivated.', LOGNOTICE)
-        except:
-            # Wenn Timeout und die Seite Offline ist
-            cConfig().setSetting('global_search_' + provider, 'false')  # deaktiviere Globale Suche
-            xbmcaddon.Addon().setSetting('plugin_' + provider + '.domain', '')  # lösche Settings Eintrag
-            log(LOGMESSAGE + ' -> [checkDomain]: Error ' + provider + ' not available.', LOGNOTICE)
-            pass
